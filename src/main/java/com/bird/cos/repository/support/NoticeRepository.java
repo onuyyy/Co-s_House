6ad877d8e7bbcd59c25cf126b5258550f9ea709c
Woong <yju407@gmail.com>
2025-09-19 12:00:06 +0900

    refactor : 관리자 db삭제로 인한 코드 수정 및 디자인 변경

--- src/main/java/com/bird/cos/repository/support/NoticeRepository.java
+++ src/main/java/com/bird/cos/repository/support/NoticeRepository.java
@@ -1,13 +1,10 @@
 package com.bird.cos.repository.support;
 
 import com.bird.cos.domain.support.Notice;
-import com.bird.cos.domain.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDateTime;
 
 @Repository
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
