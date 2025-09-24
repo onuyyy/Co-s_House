d03b7538a6666c9fd440f3047dc9dc86124af4a5
jiny <leun06@hanyang.ac.kr>
2025-09-24 14:26:12 +0900

    refactor:repository경로변경

--- src/main/java/com/bird/cos/repository/mypage/coupon/UserCouponRepository.java
+++ src/main/java/com/bird/cos/repository/mypage/coupon/UserCouponRepository.java
@@ -1,10 +1,14 @@
 package com.bird.cos.repository.mypage.coupon;
 
 import com.bird.cos.domain.coupon.UserCoupon;
+import com.bird.cos.domain.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserCouponRepository extends JpaRepository<UserCoupon, Long> {
@@ -12,4 +16,12 @@ public interface UserCouponRepository extends JpaRepository<UserCoupon, Long> {
     boolean existsByUser_UserIdAndCoupon_CouponId(Long userId, Long couponId);
 
     Page<UserCoupon> findByUser_UserId(Long userId, Pageable pageable);
+
+    @EntityGraph(attributePaths = {
+            "coupon",
+            "coupon.product",
+            "coupon.product.brand",
+            "couponStatus"
+    })
+    List<UserCoupon> findByUserAndOrderIsNull(User user);
 }
