1afa61a6f4f0f3d0b07cbbb8e670cd10057c720f
Woong <yju407@gmail.com>
2025-09-25 17:38:59 +0900

    feature: 이벤트 페이지 및 기능 구현

--- src/main/java/com/bird/cos/config/WebMvcConfig.java
+++ src/main/java/com/bird/cos/config/WebMvcConfig.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import jakarta.annotation.PostConstruct; // Spring Boot 3.x용
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration // 스프링 설정 클래스임을 명시
@@ -21,11 +22,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
         log.info("### 파일 업로드 경로 확인: {}", uploadDir); // 경로를 로그로 출력
     }
 
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        // "/images/**" URL로 요청이 들어오면
-//        // "file:///실제_파일_저장_경로/" 에서 파일을 찾도록 매핑합니다.
-//        registry.addResourceHandler("/images/**")
-//                .addResourceLocations("file:///" + uploadDir); // file:/// 주의 (슬래시 3개)
-//    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // "/images/**" URL로 요청이 들어오면
+        // "file:///실제_파일_저장_경로/" 에서 파일을 찾도록 매핑합니다.
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:///" + uploadDir); // file:/// 주의 (슬래시 3개)
+    }
 }
